3496225ddcd57ad145bd1514ee521c7440232379
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-27 19:47:58 +0000

    task 8

--- 0x00-MySQL_Advanced/7-main.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM users;
-SELECT * FROM corrections;
-
-SELECT "--";
-CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
-
-SELECT "--";
-SELECT * FROM users;
